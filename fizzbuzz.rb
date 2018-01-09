4e572c24fe6f7a8dd87f7ade60639eac7068f3f1
Kate Drennen <k8ted11@gmail.com>
2018-01-09 23:58:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@ def fizzbuzz(number) #number = 15
   #number = x
   #number = (1)upto100
   if number / 3 && number / 5
-    "Fizzbuzz"
+    "FizzBuzz"
   elsif number / 3
     "Fizz"
   elsif number / 5
