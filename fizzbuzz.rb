8b25cddfd22bd7ea9830b763dcbbe3708b52c2a8
Kate Drennen <k8ted11@gmail.com>
2018-01-09 23:58:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(number)
+def fizzbuzz(number) #number = 15
   #number = x
   #number = (1)upto100
-  if number / 3
+  if number / 3 && number / 5
+    "Fizzbuzz"
+  elsif number / 3
     "Fizz"
   elsif number / 5
     "Buzz"
-  elsif number / 3 && number / 5
-    "Fizzbuzz"
   end
 end
