8cc7f4bf5118c06c1c787738e39df03b82ad94a6
Kate Drennen <k8ted11@gmail.com>
2018-01-09 23:39:00 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,9 @@
+def fizzbuzz(number)
+  #number = x
+  #number = (1)upto100
+  if "#{number} / 3 == true)"
+  puts "Fizz"
+# elsif number / 3 == true
+  # print "Fizzbuzz"
+  # elsif number / 5 == tru  # print "Buzz"
+end
